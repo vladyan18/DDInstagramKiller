6440fa7f4bbfc667889abe77a668416f99cde6f4
Vladyan18 <Vladyan18@gmail.com>
2016-10-19 00:23:03 +0600

    created web API project

---
+++ Onstogram.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Onstogram.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Onstogram.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Onstogram.WebAPI.WebApiApplication" Language="C#" %>
